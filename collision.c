f6e2de575ec0e98a0fdb166916af146e2d906693
michael <michaeldmwu@gmail.com>
2025-11-05 15:51:02 +0000

    wtf

--- collision.c
+++ collision.c
@@ -9,41 +9,55 @@
  * @param node The node to check the distance to.
  * @return 1 if the distance is less than or equal to r, 0 otherwise.
  */
-int checkDistance(int x, int y, int r, Node *node)
+int checkDistance(long long dx, long long dy, long long r, Node *node)
 {
-    return pow((long long)pow(x - node->x, 2) + (long long)pow(y - node->y, 2), 0.5) <= (long long)pow(r, 2);
+    return dx * dx + dy * dy <= r * r;
 }
 
 /* @brief Gets the number of collisions (nodes within radius r) in the AVL tree.
+
  *
+
  * @param node The root node of the AVL tree.
+
  * @param x The x coordinate of the point.
+
  * @param y The y coordinate of the point.
+
  * @param r The radius.
+
  * @return The number of collisions.
+
  */
+
 int getNumCollisions(Node *node, int x, int y, int r)
+
 {
     if (!node)
+
         return 0;
 
     int count = 0;
 
     // check if within box of side length r*2 centered at (x, y)
-    int dx = (int)node->x - x;
-    int dy = (int)node->y - y;
+    int dx = (int)node->x - (int)x;
+    int dy = (int)node->y - (int)y;
     if (dx <= r && dx >= -1 * r && dy <= r && dy >= -1 * r)
     {
-        if (checkDistance(x, y, r, node))
+        if (checkDistance(dx, dy, r, node))
+        {
             count++;
+        }
     }
 
     // get collisions until leftmost/rightmost possible nodes
     //  go left until out of range
     if ((x - r) <= node->x)
         count += getNumCollisions(node->left, x, y, r);
+
     // go right until out of range
     if ((x + r) >= node->x)
         count += getNumCollisions(node->right, x, y, r);
+
     return count;
 }
